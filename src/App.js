08743ffd9669090ec03a69843adc6d52d1a2ad84
awolfen <alfie.wolfenden@gmail.com>
2021-11-24 16:27:48 +0000

    'basic-info'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './shared/components/FormElements/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="welcome-message">Hello, I'm Alfie!</h1>
+      <h2 className="blurb">I am developping web apps. I'm always learning.</h2>
+      <hr className="divider"/>
+      <Button href="https://github.com/awolfen" new>GitHub</Button>
+      <Button href="https://www.linkedin.com/in/alfie-wolfenden/" new>LinkedIn</Button>
+    </div >
   );
 }
 
